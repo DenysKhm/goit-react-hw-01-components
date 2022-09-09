a36f509ed4213fd23d6bc2e11fcbf572762d9d56
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-09-09 20:39:50 +0300

    render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import user from '../dataBase/user.json';
+import data from '../dataBase/data.json';
+import friends from '../dataBase/friends.json';
+import transactions from '../dataBase/transactions.json';
+
+import FriendsList from './FriendList/FriendList';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import TransactionHistory from './TransactionHistory/TransactionHistory'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile 
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      />
+      <Statistics />
+      <FriendsList/>
+      <TransactionHistory items={transactions}/>
+    </>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import FriendsListItem from "./FriendListItem"
+
+export default function FriendsList({ friends }) {
+  return (
+  <ul class="friend-list">
+<FriendsListItem/>
+  </ul>
+  )
+}
